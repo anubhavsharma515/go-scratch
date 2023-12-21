f15a50623f48a92fc54ebd2d2b38929d213e0b3e
Anubhav Sharma <anubhav.sharma@thescore.com>
2023-12-21 13:33:59 -0500

    First commit

---
+++ hell-world/hello-world.go
@@ -0,0 +1,10 @@
+// Every Go program is made up of packages.
+// Programs start running in package main.
+package main
+
+// Import path for this package is "fmt"
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+}
